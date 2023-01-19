d56c6974ad2a744c49279c4dd12ae425526c68f9
MitreshPrajapati <104007923+MitreshPrajapati@users.noreply.github.com>
2023-01-19 15:19:35 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -1,12 +1,9 @@
 
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
-require('dotenv').config();
-
-// const connection = mongoose.connect(process.env.URL, {
-//     useUnifiedTopology: true,
-//     useNewUrlParser: true
-//   });
-const connection = mongoose.connect("mongodb://127.0.0.1:27017/bugtracker");
+const dotenv = require('dotenv').config();
+// console.log(process.env.URL)
+const connection = mongoose.connect(process.env.URL);
+// const connection = mongoose.connect("mongodb://127.0.0.1:27017/bugtracker");
 
 module.exports = {connection};
