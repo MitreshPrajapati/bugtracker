18d7652a48e99754c92dc4ab53a35b302444d4ec
MitreshPrajapati <104007923+MitreshPrajapati@users.noreply.github.com>
2023-01-19 17:30:23 +0530

    final

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -7,7 +7,7 @@ const authentication = (req, res, next)=>{
     const token = JSON.parse(localStorage.getItem("token"))
 
     if(token){
-      const decodedToken = jwt.verify(token, preocess.env.SECRETKEY)
+      const decodedToken = jwt.verify(token, process.env.SECRETKEY)
       const {user_id} = decodedToken;
       req.body.user_id = user_id;
       next();
